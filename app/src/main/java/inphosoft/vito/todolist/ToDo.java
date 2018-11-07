7e7278146fb8fcca3720076838ccb4b1288223ad
Edo Oktarifa <edo.oktarifa@inphosoft.com>
2018-11-07 16:02:40 +0700

    --Wono

---
+++ app/src/main/java/inphosoft/vito/todolist/ToDo.java
@@ -0,0 +1,41 @@
+package inphosoft.vito.todolist;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class ToDo {
+
+        private UUID mID;
+        private String mActivity;
+        private Date mDate;
+
+        public ToDo() {
+            mID = UUID.randomUUID();
+            mDate = new Date();
+        }
+
+        public UUID getID() {
+            return mID;
+        }
+
+        public void setID(UUID ID) {
+            mID = ID;
+        }
+
+        public String getActivity() {
+            return mActivity;
+        }
+
+        public void setActivity(String activity) {
+            mActivity = activity;
+        }
+
+        public Date getDate() {
+            return mDate;
+        }
+
+        public void setDate(Date date) {
+            mDate = date;
+        }
+
+}
